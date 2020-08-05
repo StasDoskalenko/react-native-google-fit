fb006cd941a5b9705b6bfbfb0aa8b8c27294a939
Frikkie Snyman <frikkie@relive.cc>
2020-08-05 10:21:41 +0200

    fix typo

--- android/src/main/java/com/reactnative/googlefit/HelperUtil.java
+++ android/src/main/java/com/reactnative/googlefit/HelperUtil.java
@@ -5,9 +5,9 @@ import java.util.concurrent.TimeUnit;
 final class HelperUtil {
     public static TimeUnit processBucketUnit(String buckUnit) {
         switch (buckUnit){
-            case "NANOSECONDS": return TimeUnit.NANOSECONDS;
-            case "MICROSECONDS": return TimeUnit.MICROSECONDS;
-            case "MILLISECONDS": return TimeUnit.MILLISECONDS;
+            case "NANOSECOND": return TimeUnit.NANOSECONDS;
+            case "MICROSECOND": return TimeUnit.MICROSECONDS;
+            case "MILLISECOND": return TimeUnit.MILLISECONDS;
             case "SECOND": return TimeUnit.SECONDS;
             case "MINUTE": return TimeUnit.MINUTES;
             case "HOUR": return TimeUnit.HOURS;
